83057e38f7c46d598750129993eb1ff703ed0dee
Krystian <krystian.natkaniec@gmail.com>
2025-10-21 15:02:17 +0200

    Add Employee DTO

---
+++ src/Dto/EmployeeDto.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dto;
+
+use App\Entity\Company;
+use App\Entity\Employee;
+use Symfony\Component\ObjectMapper\Attribute\Map;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[Map(target: Employee::class)]
+class EmployeeDto
+{
+    #[Map(target: Employee::class, source: null)]
+    public ?int $id = null;
+    
+    #[Assert\NotBlank]
+    public string $firstName; 
+
+    #[Assert\NotBlank]
+    public string $lastName; 
+
+    #[Assert\NotBlank, Assert\Email]
+    public string $email; 
+
+    public ?string $phone = null;
+    
+    #[Assert\NotBlank]
+    #[Map(target: 'getCompany')]
+    public int $company;
+}
