621a941396532baf5ade77888587f342f03b7be9
Krystian <krystian.natkaniec@gmail.com>
2025-10-21 15:10:49 +0200

    Add Dockerfile and init script for PHP service

---
+++ docker/php/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+set -e
+
+until php bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1;
+do
+  sleep 1
+done
+
+echo "Create database if not exists"
+php bin/console doctrine:database:create --if-not-exists
+
+echo "Run Doctrine migrations"
+php bin/console doctrine:migrations:migrate --no-interaction
+
+exec "$@"
