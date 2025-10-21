e4ed54e04a64cd81bbf0bafbf6398825ff886768
Krystian <krystian.natkaniec@gmail.com>
2025-10-21 15:33:51 +0200

    Add permission flags for init.sh and run fixtures

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -12,4 +12,7 @@ php bin/console doctrine:database:create --if-not-exists
 echo "Run Doctrine migrations"
 php bin/console doctrine:migrations:migrate --no-interaction
 
+echo "Add fixtures"
+php bin/console doctrine:fixtures:load --no-interaction
+
 exec "$@"
