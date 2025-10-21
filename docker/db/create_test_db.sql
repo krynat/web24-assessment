f876985af4f1537377371833e349a6473fff41c8
Krystian <krystian.natkaniec@gmail.com>
2025-10-21 15:09:41 +0200

    Create test database

---
+++ docker/db/create_test_db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS `web24-assessment_api_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON `web24-assessment_api_test`.* TO 'symfony'@'%';
+FLUSH PRIVILEGES;
